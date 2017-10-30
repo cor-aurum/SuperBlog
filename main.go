a4ef0e74364b5896c4d87a64d18c67cd62c63eb8
cor-aurum <github@felix-schuetze.de>
2017-10-30 13:14:36 +0100

    Templating Engine evaluiert

--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+    "io"
+    "net/http"
+    "html/template"
+)
+
+func kekse(w http.ResponseWriter) {
+    c := http.Cookie{Name: "name", Value: "value"}
+    http.SetCookie(w, &c)
+}
+
+func startseite(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w,r,"seite/index.html",300)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "TODO Formular für Login und hinterstehende Logik" )
+}
+
+type Seite struct {
+    Titel string
+    Inhalt string
+}
+
+func seite(w http.ResponseWriter, r *http.Request) {
+    t,_ := template.ParseFiles("template.html")
+    s:=Seite{Titel:"Testseite", Inhalt:"Automatisch eingesetzter <br>Inhalt"}
+    t.Execute(w, s)
+}
 
 func main() {
-    fmt.Printf("Willkommen bei SuperBlog.\n")
+    http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css"))))
+    http.Handle("/images/", http.StripPrefix("/images", http.FileServer(http.Dir("images"))))
+    http.HandleFunc("/", startseite)
+    http.HandleFunc("/login", login)
+    http.HandleFunc("/seite/", seite)
+    http.ListenAndServe(":8000", nil)
 
 }
