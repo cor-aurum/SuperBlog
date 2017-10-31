037c199da63c8ec2b77f48bd2360aaa4510e7325
cor-aurum <github@felix-schuetze.de>
2017-10-31 14:36:58 +0100

    bisschen CSS

--- src/main/main.go
+++ src/main/main.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-    "io"
-    "net/http"
-    "html/template"
+	"html/template"
+	"io"
+	"net/http"
 )
 
 func kekse(w http.ResponseWriter) {
-    c := http.Cookie{Name: "name", Value: "value"}
-    http.SetCookie(w, &c)
+	c := http.Cookie{Name: "name", Value: "value"}
+	http.SetCookie(w, &c)
 }
 
 func startseite(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w,r,"seite/index.html",300)
+	http.Redirect(w, r, "seite/index.html", 300)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "TODO Formular für Login und hinterstehende Logik" )
+	io.WriteString(w, "TODO Formular für Login und hinterstehende Logik")
 }
 
 type Seite struct {
-    Titel string
-    Inhalt string
+	Titel  string
+	Inhalt string
+	Datum string
+	Autor string
 }
 
 func seite(w http.ResponseWriter, r *http.Request) {
-    t,_ := template.ParseFiles("template.html")
-    s:=Seite{Titel:"Testseite", Inhalt:"Automatisch eingesetzter <br>Inhalt"}
-    t.Execute(w, s)
+	t, _ := template.ParseFiles("template.html")
+	s := Seite{Titel: "Testseite", Inhalt: "Automatisch eingesetzter <br>Inhalt", Datum: "31.10.2017", Autor: "Max Mustermann"}
+	t.Execute(w, s)
 }
 
 func main() {
-    http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css"))))
-    http.Handle("/images/", http.StripPrefix("/images", http.FileServer(http.Dir("images"))))
-    http.HandleFunc("/", startseite)
-    http.HandleFunc("/login", login)
-    http.HandleFunc("/seite/", seite)
-    http.ListenAndServe(":8000", nil)
-
+	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css"))))
+	http.Handle("/images/", http.StripPrefix("/images", http.FileServer(http.Dir("images"))))
+	http.HandleFunc("/", startseite)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/seite/", seite)
+	http.ListenAndServe(":8000", nil)
 }
